87a45337ad5349bced452e7202af1e9c0ec5e2dc
Michael Schmitz <michael@schmitztech.com>
2013-05-07 08:54:50 -0700

    Add CLI parser.

--- src/main/scala/edu/knowitall/hadoop/CorpusParserMain.scala
+++ src/main/scala/edu/knowitall/hadoop/CorpusParserMain.scala
@@ -4,49 +4,56 @@ import edu.knowitall.hadoop.models._
 import com.nicta.scoobi.Scoobi._
 import java.io.File
 import edu.knowitall.tool.parse.ClearParser
+import edu.knowitall.common.Resource
+import scala.io.Source
+import java.io.PrintWriter
+import edu.knowitall.common.Timing
+import java.util.zip.GZIPOutputStream
+import java.io.BufferedOutputStream
+import java.io.FileOutputStream
 
-object CorpusParserMain extends ScoobiApp {
+object CorpusParserMain extends App {
+  final val GROUP_SIZE = 50000
 
-  def run() {
-    if (args.length != 2) usage
+  case class Config(inputFile: File, outputFile: File) {
+    def writer() = new PrintWriter(new GZIPOutputStream(new BufferedOutputStream(new FileOutputStream(outputFile))))
+    def source() = Source.fromFile(inputFile, "UTF-8")
+  }
 
-    // get the cl arguments
-    val input = args(0)
-    val output = args(1)
-
+  def run(config: Config) {
     // initialize chunker
     lazy val parser = new ClearParser()
 
     // chunk and save
-    val lines: DList[(String, Unit)] = fromTextFile(input).flatMap { line: String =>
-      try {
-        val sentence = implicitly[TabFormat[ChunkedCluewebSentence]].read(line)
-        if (sentence.text.size < 300) {
-          val parsed = parser(sentence.text)
-          Some((implicitly[TabFormat[ParsedCluewebSentence]].write(new ParsedCluewebSentence(sentence, parsed)), ()))
-        }
-        else {
-          None
+    Resource.using(config.writer()) { writer =>
+      Resource.using(config.source()) { source =>
+        for {
+          group <- source.getLines.grouped(GROUP_SIZE)
+          line <- group
+        } {
+          try {
+            val sentence = implicitly[TabFormat[ChunkedCluewebSentence]].read(line)
+            if (sentence.text.size < 300) {
+              val parsed =
+                Timing.timeThen {
+                  parser(sentence.text)
+                } { ns =>
+                  if (ns / Timing.Seconds.divisor > 1) {
+                    System.err.println("Long parse (" + Timing.Seconds.format(ns) + "): " + line)
+                  }
+                }
+              writer.synchronized {
+                writer.println(implicitly[TabFormat[ParsedCluewebSentence]].write(new ParsedCluewebSentence(sentence, parsed)))
+              }
+            }
+          } catch {
+            case e: Throwable =>
+              System.err.println("Failure on line: " + line)
+              e.printStackTrace()
+              None
+          }
         }
       }
-      catch {
-        case e: Throwable => 
-          System.err.println("Failure on line: " + line)
-          e.printStackTrace()
-          None
-      }
     }
-
-    try {
-      persist(toTextFile(lines.map(_._1), output, overwrite=false))
-    } catch {
-      case e: Throwable => e.printStackTrace()
-    }
-
-  }
-
-  def usage() {
-    System.err.println("Usage: hadoop jar <this.jar> <inputfile> <outputfile>");
-    System.exit(0);
   }
 }
